6b203e361691d99f44a8808fa30e3c0d8f217674
Greg Nelson <gjnelsonrr@gmail.com>
2023-05-02 01:18:27 -0500

    late night hope this works

--- src/app/hooks/useToDoList.ts
+++ src/app/hooks/useToDoList.ts
@@ -3,6 +3,7 @@ import { toDo } from '../types';
 import { useDispatch } from 'react-redux';
 import firebase from '@react-native-firebase/app';
 import storage from '@react-native-firebase/storage';
+import RNFetchBlob from 'react-native-fetch-blob'
 
 //what we want to do with our to dos
 
@@ -10,18 +11,13 @@ export default () => {
   const dispatch = useDispatch();
 
   const getToDos = async () => {
-    try {
-      const downloadTo = `${firebase.utils.FilePath.DOCUMENT_DIRECTORY}/todos.json`;
-      const todos = storage().ref('todos.json').writeToFile(downloadTo);
-
-      if (todos != null) {
-        console.log('loading data' + JSON.stringify(todos));
-        const loadedToDos: Array<toDo> = JSON.parse(todos.toString());
-        dispatch(load(loadedToDos));
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    RNFetchBlob.fetch("GET", "gs://todo-9fca6.appspot.com/todos.json", {
+    Authorization: "c5f51f78-5356-4236-8438-3b10b14e1cff",
+  })
+  .then((res) => {
+      let json = res.json();
+      dispatch(load(json));
+  })
   };
 
   const saveToDos = async (todos: any) => {
